457412ddea081dcae76d1060c30cca5438dccbb5
ioannis <ioannis.tsiokos@gmail.com>
2023-04-16 16:42:25 +0300

    fixed import error

--- services/supabase.py
+++ services/supabase.py
@@ -1,5 +1,5 @@
 import os
-from supabase import create_client, Client, APIResponse
+from supabase import create_client, Client
 from typing import List
 
 url: str = os.environ.get("SUPABASE_URL")
@@ -7,7 +7,7 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 def query_question_embeddings(chain: str) -> List[List[float]]:
-    response: APIResponse = supabase.table('idx_stakex_cms_questions_chain').select('embedding').eq('chain', chain).execute()
+    response = supabase.table('idx_stakex_cms_questions_chain').select('embedding').eq('chain', chain).execute()
     results: List[List[float]] = []
     for entry in response.data:
         if entry.embedding == None or entry.embedding == "":
