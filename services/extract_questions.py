ad239421202a5550d57ae514833b6a1865f41803
ioannis <ioannis.tsiokos@gmail.com>
2023-04-17 15:09:56 +0300

    added min length

--- services/extract_questions.py
+++ services/extract_questions.py
@@ -3,11 +3,15 @@ import json
 from typing import Dict
 
 def extract_questions_from_text(text: str, question_count: int = 3) -> Dict[str, str]:
+    if len(text) < 50:
+        return []
+    
     messages = [
         {
             "role": "system",
             "content": f"""
             Given some text from a user, try to come up with the top {question_count} questions that this text answers.
+            Questions must be succinct. Do not include people or user names in the questions.
             Respond with a line-separated list of the questions.
             """,
         },
@@ -22,6 +26,7 @@ def extract_questions_from_text(text: str, question_count: int = 3) -> Dict[str,
 
     try:
         questions = [q.lstrip('0123456789.- ') for q in completion.splitlines()]
+        questions = list(filter(lambda i: len(i) > 7, questions))
     except:
         questions = []
 
