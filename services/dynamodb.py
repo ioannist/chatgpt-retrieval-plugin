3f56515eedf2e598aeda4099f95dc2e337f0b215
itsio <ioannis.tsiokos@gmail.com>
2023-05-23 18:21:39 +0300

    added questionEdited continue

--- services/dynamodb.py
+++ services/dynamodb.py
@@ -212,6 +212,9 @@ def query_and_save_questions():
     )
     while True:
         for entry in response.get('Items', []):
+            questionEdited = entry.get('questionEdited')
+            if questionEdited == '' or questionEdited == None:
+                continue
             question = entry.get("question")
             print(question)
             embedding = entry.get("embedding")
